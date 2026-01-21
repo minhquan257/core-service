6939ba4a58afbcbf20b2b69bb884c9742217fa25
minhquan257 <20520710@gm.uit.edu.vn>
2026-01-21 23:31:55 +0700

    feat: add configuration and database setup with TypeORM and Swagger integration
    - Added @nestjs/config for environment variable management.
    - Integrated TypeORM with PostgreSQL for database connectivity.
    - Configured global settings for the ConfigModule.
    - Set up Swagger documentation for the API.
    - Updated package.json with new dependencies for config, typeorm, and swagger.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || '1',
+      database: process.env.DB_DATABASE || 'core_service',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
