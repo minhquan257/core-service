35a4c16c0ec6570369d97a607053c1b4060ee64e
Quan Nguyen Minh <quan.nguyen@polaris-edge.com>
2026-04-02 23:26:14 +0700

    feat: fix M3 and M8 by helmet and cors to deploy

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,22 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+
+  app.use(helmet({
+    hsts: {
+      maxAge: 31536000, // 1 năm
+      includeSubDomains: true,
+      preload: true,
+    },
+  }));
+
+  app.enableCors({
+    origin: true, 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  
   SwaggerModule.setup('', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
